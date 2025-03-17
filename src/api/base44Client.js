18baee47078e0ab2207bf8cbf3d04f7e87b1fff5
UrielSabah <urielsabahsoundeng@gmail.com>
2025-03-18 01:07:21 +0200

    update some fixes

--- src/api/base44Client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@base44/sdk';
-// import { getAccessToken } from '@base44/sdk/utils/auth-utils';
-
-// Create a client with authentication required
-export const base44 = createClient({
-  appId: "67d830b192dca4c85883f02d", 
-  requiresAuth: true // Ensure authentication is required for all operations
-});
